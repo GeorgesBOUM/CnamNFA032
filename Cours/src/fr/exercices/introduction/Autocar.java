3d755f3a27e762d3fac606c5e90b15be7dce837c
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-06 19:33:09 +0100

    le main, pour le lancement de quelques tests

--- Cours/src/fr/exercices/introduction/Autocar.java
+++ Cours/src/fr/exercices/introduction/Autocar.java
@@ -3,7 +3,15 @@ package fr.exercices.introduction;
 public class Autocar {
 
 	public static void main(String[] args) {
-		
+		String [] tabVilles = {"Vierzon", "Salbris", "Nouans", "Lamotte-Beuvron",
+				"La Ferté Saint-Aubin", "Orléans"};
+		double [] tabPrix = {3.20, 1.80, 2.30, 4.20, 5.00};
+		String depart = "Vierzon", arrivee = "Orléans";
+		System.out.println("indice " + arrivee + " = " + Autocar.recupereIndiceVille(arrivee, tabVilles));
+		System.out.println("Le prix du trajet entre " + depart + " et " 
+				+ arrivee + " est "
+				+ Autocar.prixEntreDeuxVilles(depart, arrivee, tabPrix, tabVilles)
+				+ "€");
 	}
 	
 	public static int recupereIndiceVille(String nomVille, String [] tabVilles) {
@@ -24,8 +32,8 @@ public class Autocar {
 		int indiceArrivee = Autocar.recupereIndiceVille(arrivee, tabVilles);
 		double prixTrajet = 0;
 		int reduction = 0;
-		for (int i = indiceDepart; i <= indiceArrivee; i++) {
-			prixTrajet = prixTrajet + tabPrix[i] - tabPrix[i]*(reduction/100);
+		for (int i = indiceDepart; i < indiceArrivee; i++) {
+			prixTrajet = prixTrajet + tabPrix[i] - (tabPrix[i]*(reduction/100));
 			reduction += 10;
 		}
 		return prixTrajet;
