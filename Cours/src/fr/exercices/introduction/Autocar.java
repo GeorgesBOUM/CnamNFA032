3b72979c187fd70559248c1bc4b5400c414b3a2a
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-06 07:05:11 +0100

    récupération indice ville dans tableau

--- Cours/src/fr/exercices/introduction/Autocar.java
+++ Cours/src/fr/exercices/introduction/Autocar.java
@@ -6,5 +6,15 @@ public class Autocar {
 		
 	}
 	
-	
+	public static int recupereIndiceVille(String nomVille, String [] tabVilles) {
+		int indice = -1;
+		boolean trouve =  false;
+		while (indice < tabVilles.length && !trouve) {
+			indice++;
+			if (nomVille.equals(tabVilles[indice])) {
+				trouve = true;
+			}
+		}
+		return indice;
+	}
 }
