ee1520fc429078c7de824185b32bf800aaeba92a
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-06 20:31:18 +0100

    crible simple

---
+++ Cours/src/fr/exercices/introduction/CribleErathosthene.java
@@ -0,0 +1,24 @@
+package fr.exercices.introduction;
+
+public class CribleErathosthene {
+
+	public static void main(String[] args) {
+	}
+	
+	public static boolean [] cribleSimple(int n) {
+		boolean [] tab = new boolean[n+1];
+		tab[0] = false;
+		tab[1] = false;
+		for (int i = 2; i <= n; i++) {
+			tab[i] = true;
+		}
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (tab[i]) {
+				for (int j = 2; i*j <= n; j++) {
+					tab[i*j] = false;
+				}
+			}
+		}
+		return tab;
+	}
+}
