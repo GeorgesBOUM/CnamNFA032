4f16b457ece0a60421f1c045305887fae3f9351b
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-16 05:32:16 +0100

    creation de quelques tableaux

---
+++ Cours/src/fr/exercices/references/Tableaux.java
@@ -0,0 +1,19 @@
+package fr.exercices.references;
+
+public class Tableaux {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] t1, t2, t3, t4, t5;
+		t2 = new int[4];
+		t3 = new int[4];
+		t4 = new int[4];
+		for (int i=0; i<t3.length; i++){
+		t3[i] = 10+i;
+		t4[i] = 10+i;
+		}
+		t5 = t4;
+		
+	}
+
+}
