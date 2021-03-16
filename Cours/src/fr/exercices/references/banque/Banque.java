c15629d46b8a9f69d84ece94cfcd7252e997914f
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-16 16:59:17 +0100

    possibilité d'ajouter un compte à une banque

--- Cours/src/fr/exercices/references/banque/Banque.java
+++ Cours/src/fr/exercices/references/banque/Banque.java
@@ -1,13 +1,21 @@
 package fr.exercices.references.banque;
 
+import java.util.ArrayList;
+
 public class Banque {
 	private String nom;
+	private ArrayList<Compte> listeComptes;
 	
 	public Banque(String nom) {
 		this.nom = nom;
+		listeComptes = new ArrayList<Compte>();
 	}
 	
 	public String getNom() {
 		return nom;
+	}
+	
+	public void ajouterCompte(Compte compte) {
+		listeComptes.add(compte);
 	}
 }
