0c00f18dfb87daface18cfc36c9358de0bae42c8
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-16 16:31:29 +0100

    classe Banque: création

---
+++ Cours/src/fr/exercices/references/banque/Banque.java
@@ -0,0 +1,13 @@
+package fr.exercices.references.banque;
+
+public class Banque {
+	private String nom;
+	
+	public Banque(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+}
