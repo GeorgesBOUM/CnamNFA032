481a8c6d8e1624a508041af3f03221c0635aeba9
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-17 05:45:37 +0100

    possibilité d'ajouter plusieurs titulaires à un compte

--- Cours/src/fr/exercices/references/banque/Compte.java
+++ Cours/src/fr/exercices/references/banque/Compte.java
@@ -1,23 +1,36 @@
 package fr.exercices.references.banque;
 
+import java.util.ArrayList;
+
 public class Compte {
 	private int numero;
 	private double solde;
+	private ArrayList<Titulaire> listeDesTitulaires;
 	
 	public Compte(int numero, double solde) {
 		this.numero = numero;
 		this.solde = solde;
+		this.listeDesTitulaires = new ArrayList<Titulaire>();
 	}
 	
 	public double getSolde() {
 		return solde;
 	}
+	
+	public ArrayList<Titulaire> getListeDesTitulaires(){
+		return listeDesTitulaires;
+	}
+	
 	public void depot(double d) {
 		this.solde += d;
 	}
 	
 	public void retrait(double r) {
 		this.solde -= r;
+	}
+	
+	public void ajouterTitulaire(Titulaire titulaire) {
+		listeDesTitulaires.add(titulaire);
 	}
 	
 	public String toString() {
