20b840f1038fd1e0d648b2aab4df7241c3914c96
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-19 05:47:27 +0100

    creation de quelques clients de la banque

--- Cours/src/fr/exercices/references/banque/Application.java
+++ Cours/src/fr/exercices/references/banque/Application.java
@@ -4,6 +4,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		Banque banque = Banque.getInstance();
+		
+		Titulaire paul = new Titulaire("Paul");
+		Titulaire pierre = new Titulaire("Pierre");
+		Titulaire fatima = new Titulaire("fatima");
 	}
 
 }
