12fe779de34549ab069d75cf562f0a00d3395273
GeorgesBOUM <boumgeorges@protonmail.com>
2021-03-19 06:46:12 +0100

    mise en stand by

--- Cours/src/fr/exercices/references/banque/Application.java
+++ Cours/src/fr/exercices/references/banque/Application.java
@@ -1,5 +1,8 @@
 package fr.exercices.references.banque;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Application {
 
 	public static void main(String[] args) {
@@ -8,6 +11,14 @@ public class Application {
 		Titulaire paul = new Titulaire("Paul");
 		Titulaire pierre = new Titulaire("Pierre");
 		Titulaire fatima = new Titulaire("fatima");
+		ArrayList<Titulaire> conjoints = new ArrayList<Titulaire>();
+		conjoints.add(paul);
+		conjoints.add(fatima);
+		
+		Compte comptePaul = new Compte(1, paul, 100);
+		Compte comptePierre = new Compte(100);
+		Compte compteFatima = new Compte(100);
+		Compte comptePaulEtFatima = new Compte(50);
 	}
 
 }
